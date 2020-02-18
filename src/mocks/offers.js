0bc2eea49ab7b7202690574072ace9990c826108
unknown <@15ISPOLATOV.tservicegroup.ru>
2020-02-18 08:05:59 +0300

    #JS3-3-1 add mocks

---
+++ src/mocks/offers.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    name: `Beautiful & luxurious apartment at great location`,
+    img: `img/apartment-01.jpg`,
+    price: 120,
+    rating: 80,
+    type: `Apartment`,
+    period: `night`,
+    premium: true
+  },
+  {
+    name: `Wood and stone place`,
+    img: `img/room.jpg`,
+    price: 80,
+    rating: 70,
+    type: `Private room`,
+    period: `night`,
+    premium: false
+  },
+  {
+    name: `Canal View Prinsengracht`,
+    img: `img/apartment-02.jpg`,
+    price: 132,
+    rating: 50,
+    type: `Apartment`,
+    period: `night`,
+    premium: true
+  },
+  {
+    name: `Nice, cozy, warm big bed apartment`,
+    img: `img/apartment-03.jpg`,
+    price: 180,
+    rating: 100,
+    type: `Apartment`,
+    period: `night`,
+    premium: false
+  },
+];
